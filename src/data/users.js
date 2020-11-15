a3b720bd46e1bbeb131f7cf0713281f0e8b7452a
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-11-15 09:59:09 -0300

    routing config, adding mock userdata

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    name: 'João Silva',
+    email: 'josil@email.com',
+    avatarUrl: 'https://i.pravatar.cc/300',
+  },
+  {
+    id: 2,
+    name: 'Mariana aguiar',
+    email: 'magui@email.com',
+    avatarUrl: 'https://i.pravatar.cc/300',
+  },
+  {
+    id: 3,
+    name: 'Juliana Fragoso',
+    email: 'julianafrag@email.com',
+    avatarUrl: 'https://i.pravatar.cc/300',
+  },
+  {
+    id: 4,
+    name: 'Rafael Monteiro',
+    email: 'ramon@email.com',
+    avatarUrl: 'https://i.pravatar.cc/300',
+  },
+];
